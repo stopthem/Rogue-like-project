84a48a417ff091e0a15ae13a7ef1a1bcbcbfdb6b
cngkts <goktascan1@gmail.com>
2020-09-11 21:53:50 +0300

    added levels-rooms.

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public GameObject[] doors;
+    public bool closeWhenEntered, openWhenEnemiesCleared;
+    public List<GameObject> enemies = new List<GameObject>();
+    private bool roomActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (enemies.Count > 0 && roomActive && openWhenEnemiesCleared)
+        {
+            for (int i = 0; i < enemies.Count; i++)
+            {
+                if (enemies[i] == null)
+                {
+                    enemies.RemoveAt(i);
+                    i--;
+                }
+            }
+            if (enemies.Count == 0)
+            {
+                
+                foreach (GameObject door in doors)
+                {
+                    openWhenEnemiesCleared = true;
+                    door.SetActive(false);
+                    closeWhenEntered =false;
+                }
+
+            }
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            CameraController.instance.ChangeTraget(transform);
+            
+            if (closeWhenEntered)
+            {
+                foreach (GameObject door in doors)
+                {
+                    door.SetActive(true); 
+                }
+            }
+            roomActive = true;
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            roomActive = false;
+        }
+    }
+}
